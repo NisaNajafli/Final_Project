49e54df8fdba819ceb2ece947f1f27340db6c99e
NisaNacafova <ncfliniss@gmail.com>
2023-07-04 14:52:24 +0400

    Change

---
+++ DataAccess/Migrations/20230704105033_ChangeTaxes.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class ChangeTaxes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Informations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Taxs",
+                table: "Taxs");
+
+            migrationBuilder.RenameTable(
+                name: "Taxs",
+                newName: "Taxes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Taxes",
+                table: "Taxes",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Taxes",
+                table: "Taxes");
+
+            migrationBuilder.RenameTable(
+                name: "Taxes",
+                newName: "Taxs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Taxs",
+                table: "Taxs",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Informations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmploymentOfSpouse = table.Column<int>(type: "int", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsMale = table.Column<bool>(type: "bit", nullable: false),
+                    IsMarried = table.Column<bool>(type: "bit", nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoOfChildren = table.Column<int>(type: "int", nullable: false),
+                    Religion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Informations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Informations_AspNetUsers_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Informations_EmployeeId",
+                table: "Informations",
+                column: "EmployeeId");
+        }
+    }
+}
